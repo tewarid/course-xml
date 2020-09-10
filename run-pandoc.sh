82e33b069a62684ccb4edd8d807fc0e4a50faca2
Devendra Tewari <devendra.tewari@gmail.com>
2020-09-10 17:30:05 -0300

    update pandoc docker image

--- run-pandoc.sh
+++ run-pandoc.sh
@@ -1,5 +1,5 @@
-files=(front-matter.md xml.md dtd.md schema.md dom.md sax.md css.md xsl.md)
+#!/bin/sh
 # create standalone revealjs presentation
-pandoc -F mermaid-filter --slide-level=2 -o slides.html -t revealjs -s -V minScale=0.2 -V maxScale=1.5 -V width=\"100%\" -V height=\"150%\" -V margin=0.05 ${files[@]}
+pandoc -F mermaid-filter --slide-level=2 -o slides.html -t revealjs -s -V minScale=0.2 -V maxScale=1.5 -V width=\"100%\" -V height=\"150%\" -V margin=0.05 front-matter.md xml.md dtd.md schema.md dom.md sax.md css.md xsl.md
 # create PowerPoint
-pandoc -F mermaid-filter --slide-level=2 -o slides.pdf -t beamer ${files[@]}
+pandoc -F mermaid-filter --slide-level=2 -o slides.pdf -t beamer front-matter.md xml.md dtd.md schema.md dom.md sax.md css.md xsl.md
